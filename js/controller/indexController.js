ca70fc71085a92215bbd0cbf27524993a24fc2ea
Biagio Boi <biagioboi@gmail.com>
2020-04-09 18:51:56 +0200

    started firebase support and fixed index_planning.html page, removed all duplicates

--- js/controller/indexController.js
+++ js/controller/indexController.js
@@ -1,33 +1,49 @@
 const ADDRESS = "localhost/proj_tesi_backend"; // server address
 const BACKGROUND_COLOR = {run: 'btn-success', waiting: 'btn-warning', error: 'btn-danger', off: 'btn-secondary'}; // machine status background color
+const IMG_STATUS = {
+    run: 'img/img-status-machine/productivity.png',
+    warning: 'img/img-status-machine/warning.png',
+    error: 'img/img-status-machine/brokenblack.png',
+    off: 'img/img-status-machine/offwhite.png'
+};
+// img_status not considered: change.png, clock.png
+// maybe we can change machine image too, depending on which class of machine we're considering
 
 $(document).ready(() => {
-    machines = loadMachineFromDatabase();
-    sessionStorage.setItem("storedMachines", JSON.stringify(machines));
-    loadMachine(machines);
+
+    // firebase configuration
+    var firebaseConfig = {
+        apiKey: "AIzaSyCTMjXyQXquU_OL7-nrJxptjEoem3A-Vgk",
+        authDomain: "tesi-75dcc.firebaseapp.com",
+        databaseURL: "https://tesi-75dcc.firebaseio.com",
+        projectId: "tesi-75dcc",
+        storageBucket: "tesi-75dcc.appspot.com",
+        messagingSenderId: "927587508970",
+        appId: "1:927587508970:web:79bd42f77acab26927ad93"
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    sessionStorage.setItem("storedMachines", JSON.stringify(loadMachineFromDatabase(loadMachine)));
 });
 
 
 /**
  * Retrieve machines from database
- * Path: Output/machines_config.txt
  */
-function loadMachineFromDatabase() {
+function loadMachineFromDatabase(callback) {
     let listMachines = [];
-    $.ajax({
-        async: false,
-        url: 'http://' + ADDRESS + '/Output/machines_config.txt',
-        success: (response) => {
-            response = JSON.parse(response);
-            response.forEach((e) => {
-                let oee = new OEE(e.oee.general, e.oee.availability, e.oee.performance, e.oee.quality);
-                let x = new Machine(e.name, e.status, e.powerOn, e.start, e.beatingSpeedPerHours, e.startMouldAssembly, e.endMouldAssembly, e.changeOfWorkingShift, e.secondChangeOfWorkingShift, oee);
-                listMachines.push(x);
-            });
-        },
-        error: (response) => {
-            alert("We run into an error.")
-        }
+    let db = firebase.firestore();
+    let collection = db.collection("macchinario");
+    collection.get().then((querySnapshot) => {
+        querySnapshot.forEach((e) => {
+            e = e.data();
+            let oee = new OEE(e.oee.general, e.oee.availability, e.oee.performance, e.oee.quality);
+            let x = new Machine(e.name, e.status, e.powerOn, e.start, e.beatingSpeedPerHours, e.startMouldAssembly, e.endMouldAssembly, e.changeOfWorkingShift, e.secondChangeOfWorkingShift, oee);
+            listMachines.push(x);
+        });
+        callback(listMachines)
     });
     return listMachines;
 }
@@ -37,16 +53,8 @@ function loadMachineFromDatabase() {
  * @param machines array of machines
  */
 function loadMachine(machines) {
-    let content;
-    $.ajax({
-        async: false,
-        url: "content/cardMachine.html",
-        success: (response) => {
-            content = response;
-        }
-    });
+    let content = $("#preCharge").html();
     machines.forEach((e) => {
-        $("#preCharge").html(content);
         $("#preCharge .div-resume-status-machine").attr('machineid', e.name);
         $("#preCharge .machine-name").html(e.name);
         $("#preCharge .machine-status").html(e.status);
@@ -54,7 +62,9 @@ function loadMachine(machines) {
         $("#preCharge .progress-bar-oee").attr("aria-valuenow", e.oee.general);
         $("#preCharge .progress-bar-oee").css("width", e.oee.general + "%");
         $("#preCharge .principal-div-machine").addClass(BACKGROUND_COLOR[e.status.toLowerCase()]);
+        $("#preCharge .img-status-machine").attr('src', IMG_STATUS[e.status.toLowerCase()]);
         $("#machineList").append($("#preCharge").html());
+        $("#preCharge").html(content);
     });
 }
 
